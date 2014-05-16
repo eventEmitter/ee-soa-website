2dc961f93619474a9aff9ffc60b9eef000dfe22d
Michael van der Weg <michael@joinbox.com>
2014-05-16 14:41:32 +0200

    firs tuntested version

---
+++ README.md
@@ -0,0 +1,14 @@
+# ee-soa-webstie
+
+description
+
+## installation
+
+
+
+## build status
+
+[![Build Status](https://travis-ci.org/eventEmitter/ee-soa-webstie.png?branch=master)](https://travis-ci.org/eventEmitter/ee-soa-webstie)
+
+
+## usage
---
+++ test/default.js
@@ -0,0 +1,21 @@
+
+	
+	var   Class 		= require('ee-class')
+		, log 			= require('ee-log')
+		, assert 		= require('assert')
+		, travis 		= require('ee-travis');
+
+
+
+	var Something = require('../')
+
+
+
+
+
+	describe('something ', function(){
+		it('should ', function(){
+
+		});
+	});
+	
